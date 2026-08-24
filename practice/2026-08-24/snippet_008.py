de22fc4781a37a50b63ae22f7277269f9a51f2f9
avanishkasar <avanishkasar@gmail.com>
2026-08-24 05:35:39 +0000

    practice: revised f-strings and string formatting

---
+++ practice/2026-08-24/snippet_008.py
@@ -0,0 +1,8 @@
+# Practice — commit 8
+# Topic: revised f-strings and string formatting
+# Time: 05-35-39 UTC
+
+# count duplicates
+from collections import Counter
+nums = [1,1,2,3,3,3]
+print(Counter(nums))
