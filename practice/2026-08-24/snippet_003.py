b40fb55837ba5ab6ee08b336d22e1d2e25fa2565
avanishkasar <avanishkasar@gmail.com>
2026-08-24 05:35:12 +0000

    study: practiced breaking problems into smaller steps before coding

---
+++ practice/2026-08-24/snippet_003.py
@@ -0,0 +1,7 @@
+# Practice — commit 3
+# Topic: practiced breaking problems into smaller steps before coding
+# Time: 05-35-12 UTC
+
+pairs = [(1,'a'),(2,'b'),(3,'c')]
+for i, ch in pairs:
+    print(i, ch)
