417bcbb754253db78ac7eb25b7c278d3c78d4567
avanishkasar <avanishkasar@gmail.com>
2026-08-27 16:06:03 +0000

    study: practiced breaking problems into smaller steps before coding

---
+++ practice/2026-08-27/snippet_019.py
@@ -0,0 +1,6 @@
+# Practice — commit 19
+# Topic: practiced breaking problems into smaller steps before coding
+# Time: 16-06-03 UTC
+
+nums = [2, 4, 6, 8, 10]
+print(list(filter(lambda x: x % 4 == 0, nums)))
