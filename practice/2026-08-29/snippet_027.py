7b8febd4a20ec519a1491e61c7165d705b18b145
avanishkasar <avanishkasar@gmail.com>
2026-08-29 11:30:28 +0000

    drill: practiced greedy algorithm thinking

---
+++ practice/2026-08-29/snippet_027.py
@@ -0,0 +1,8 @@
+# Practice — commit 27
+# Topic: practiced greedy algorithm thinking
+# Time: 11-30-28 UTC
+
+# count duplicates
+from collections import Counter
+nums = [1,1,2,3,3,3]
+print(Counter(nums))
