84e58629fcdd84b8a2ce4642cc19a64ce0ebceb3
avanishkasar <avanishkasar@gmail.com>
2026-09-04 09:19:01 +0000

    practice: reviewed two-pointer technique basics

---
+++ practice/2026-09-04/snippet_09-19-01_015.py
@@ -0,0 +1,8 @@
+# Practice commit 15
+# Topic: reviewed two-pointer technique basics
+
+stk = []
+for ch in '({[]})':
+    if ch in '({[': stk.append(ch)
+    else: stk.pop() if stk else None
+print(not stk)
