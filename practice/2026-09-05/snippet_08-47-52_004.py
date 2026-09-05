def07fe706cc7894932eac2a218ec34496fa82ff
avanishkasar <avanishkasar@gmail.com>
2026-09-05 08:47:52 +0000

    practice: practiced range() with start stop and step

---
+++ practice/2026-09-05/snippet_08-47-52_004.py
@@ -0,0 +1,8 @@
+# Practice commit 4
+# Topic: practiced range() with start stop and step
+
+stk = []
+for ch in '({[]})':
+    if ch in '({[': stk.append(ch)
+    else: stk.pop() if stk else None
+print(not stk)
