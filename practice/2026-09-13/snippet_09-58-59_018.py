81ac7ce8403f5e4e9dc065c5f3a303e68241eda1
avanishkasar <151715253+avanishkasar@users.noreply.github.com>
2026-09-13 09:58:59 +0000

    practice: revised return vs print: key difference in functions

---
+++ practice/2026-09-13/snippet_09-58-59_018.py
@@ -0,0 +1,8 @@
+# Practice commit 18
+# Topic: revised return vs print: key difference in functions
+
+nums, target = [2,7,11,15], 9
+seen = {}
+for i, n in enumerate(nums):
+    if target - n in seen: print(seen[target-n], i)
+    seen[n] = i
